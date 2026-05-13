0fca6c62de09c233912b95786dbcbc9a215688ee
emjaycodes <justicethedev@gmail.com>
2026-05-13 09:50:04 +0100

    fix: correct key name for averageRatings in ProviderRatings model

--- lib/features/settings/data/models/provider_ratings.dart
+++ lib/features/settings/data/models/provider_ratings.dart
@@ -13,7 +13,7 @@ class ProviderRatings {
 
   factory ProviderRatings.fromJson(Map<String, dynamic> json) =>
       ProviderRatings(
-        averageRatings: json['averageRatings'] as int?,
+        averageRatings: json['averageRating'] as int?,
         totalReviews: json['totalReviews'] as int?,
         reviews: json['reviews'] == null
             ? <ProviderReview>[]
