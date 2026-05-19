a52978733d16a29b863758901069b110b7298297
emjaycodes <justicethedev@gmail.com>
2026-05-19 18:12:03 +0100

    fix: restore release build configuration in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -50,23 +50,17 @@ android {
         }
     }
 
-    // buildTypes {
-    //     release {
-          
-    //         signingConfig = signingConfigs.getByName("release")
-    //         isShrinkResources = true
-    //         isMinifyEnabled = true
-    //         proguardFiles(
-    //             getDefaultProguardFile("proguard-android-optimize.txt"),
-    //             "proguard-rules.pro"
-    //         )
-    //     }
-    // }
-
     buildTypes {
-    release {
-        signingConfig = signingConfigs.getByName("debug")
-    }
+        release {
+          
+            signingConfig = signingConfigs.getByName("release")
+            isShrinkResources = true
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
     }
 }
 
